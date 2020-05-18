13528598f23edf2f22967ddbd7078691139dab67
Aldahir Ortega Rojas <aldahir5010@gmail.com>
2020-05-18 01:35:29 -0500

    Patrón MVT: Modelo-Vista-Template

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -14,16 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path 
+from core import views as core_views
+from portfolio import views as portfolio_views
 
-from django.conf import settings  # <=====
+from django.conf import settings
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('about-me/', views.about, name="about"),
-    path('portfolio/', views.portfolio, name="portfolio"),
-    path('contact/', views.contact, name="contact"),
+    path('', core_views.home, name="home"),
+    path('about-me/', core_views.about, name="about"),
+    path('portfolio/', portfolio_views.portfolio, name="portfolio"),
+    path('contact/', core_views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
 
