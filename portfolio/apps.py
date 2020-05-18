dd570139c73a0ddb7b84828f745b33b35da094ed
Aldahir Ortega Rojas <aldahir5010@gmail.com>
2020-05-18 01:12:53 -0500

    Personalizando el administrador (1)

--- portfolio/apps.py
+++ portfolio/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
 class PortfolioConfig(AppConfig):
     name = 'portfolio'
+    verbose_name = 'Portafolio'
--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -17,10 +17,17 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+from django.conf import settings  # <=====
+
 urlpatterns = [
-    path('',views.home, name="home"),
-    path('about-me/',views.about, name="about"),
-    path('portfolio/',views.portfolio, name="portfolio"),
-    path('contact/',views.contact, name="contact"),
+    path('', views.home, name="home"),
+    path('about-me/', views.about, name="about"),
+    path('portfolio/', views.portfolio, name="portfolio"),
+    path('contact/', views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, 
+        document_root=settings.MEDIA_ROOT)
